fa720fb0d046913c4b6e72dc082d04b977597564
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2023-03-27 12:00:37 +0300

    Updates

---
+++ precache-manifest.574e1244ac3b1f24e742f87d1b67d476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e776df130c688ad0040f45ff4d718b",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "8bc3ea54f923b1471a48",
+    "url": "/todoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "d57ec5e2244984068b14",
+    "url": "/todoList/static/js/2.30cdd6ea.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoList/static/js/2.30cdd6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc3ea54f923b1471a48",
+    "url": "/todoList/static/js/main.77cff814.chunk.js"
+  },
+  {
+    "revision": "194eb4b6959270b5367a",
+    "url": "/todoList/static/js/runtime-main.1bfd27b7.js"
+  }
+]);
